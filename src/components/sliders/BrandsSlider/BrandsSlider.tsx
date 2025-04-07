6a861b9f8dc73312011e4e66edac35d55165058d
Andas-LV <andassatybaldy@gmail.com>
2025-04-07 19:10:54 +0500

    refactored: sliders to tailwind

--- src/components/sliders/BrandsSlider/BrandsSlider.tsx
+++ src/components/sliders/BrandsSlider/BrandsSlider.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import styles from "./BrandsSlider.module.css";
 import { CarouselItem } from "@components/ui/carousel";
 import ReusableCarousel from "@components/ui/ReusableCarousel";
 import { exampleBrands } from "@/exampleData/exampleFilters";
 
 export default function BrandsSlider() {
 	return (
-		<div className={styles.BrandsSlider}>
+		<div className={"mt-4"}>
 			<ReusableCarousel slidesToScroll={3}>
 				{exampleBrands.map((brand, i) => (
-					<CarouselItem key={i} className={styles.brandItem}>
+					<CarouselItem
+						key={i}
+						className="flex-none bg-[var(--brand-gray)]
+						py-3 px-4 rounded-[1rem] font-bold
+						text-[14px] cursor-pointer
+						hover:bg-[var(--skeleton)]"
+					>
 						{brand.label}
 					</CarouselItem>
 				))}
--- src/components/sliders/PopularCategoriesSlider/PopularCategoriesSlider.tsx
+++ src/components/sliders/PopularCategoriesSlider/PopularCategoriesSlider.tsx
@@ -11,7 +11,15 @@ export default function PopularCategoriesSlider() {
 
 			<ReusableCarousel slidesToScroll={1}>
 				{popularCategories.map((category, i) => (
-					<CarouselItem key={i} className={styles.categoryItem}>
+					<CarouselItem
+						key={i}
+						className="flex flex-col items-center gap-4
+						flex-none font-bold rounded-[1rem] border
+						border-gray-300 p-4
+						shadow-[0_4px_10px_rgba(0,0,0,0.1)]
+						cursor-pointer
+						hover:shadow-[0_0_10px_rgba(0,0,0,0.3)]"
+					>
 						<img
 							src={category.image}
 							alt={category.name}
